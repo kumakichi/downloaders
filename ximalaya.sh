8a7f0f911db1007af3219e86d2f742192e2d5010
cm_san <xbapj1@gmail.com>
2015-06-08 15:31:02 +0800

    modified

--- ximalaya.sh
+++ ximalaya.sh
@@ -15,11 +15,13 @@ PAGE_URL=$1
 TMP_JSON=$(mktemp)
 TRACK_NUM=${PAGE_URL##*/}
 wget -c ${JSON_PREFIX}/${TRACK_NUM}".json" -O $TMP_JSON
-SUFFIX=$(awk -F',' '{print $2}' $TMP_JSON | grep $SIGN | awk -F':' '{print $2}' | tr -d '"')
-
-OUT_FILE=${TRACK_NUM}".mp3"
-DL_LINK=${BASE}/${SUFFIX}
-echo $DL_LINK
+#SUFFIX=$(awk -F',' '{print $2}' $TMP_JSON | grep $SIGN | awk -F':' '{print $2}' | tr -d '"')
+#
+#OUT_FILE=${TRACK_NUM}".mp3"
+#DL_LINK=${BASE}/${SUFFIX}
+DL_LINK=$(awk -F',' '{print $2}' $TMP_JSON | grep $SIGN | awk -F'":"' '{print $2}' | tr -d '"')
+OUT_FILE=${TRACK_NUM}"."${DL_LINK##*.}
+echo "Downloading from $DL_LINK ..."
 
 wget -c $DL_LINK -O $OUT_FILE
 
